fb64dc16d33ef74a973e58929286db994b4ddfe3
PG <scottpiliben@gmail.com>
2018-01-18 19:26:25 +0800

    add common for use

--- tree_Lzero/tree_first_version.py
+++ tree_Lzero/tree_first_version.py
@@ -17,8 +17,8 @@ def main ():
     #import OLS_linear_regression as OLS
     import numpy as np
     import math 
-    q=Queue()
-    q_br=Queue()
+    q=Queue() # child single trail
+    q_br=Queue() # branch output local information 
     my_path = input ('輸入你的路徑:')
     os.chdir(my_path) #切換路徑
     d=input("輸入你要的檔名.txt: ") 
@@ -47,20 +47,23 @@ def main ():
         bran=infor[1]
         bran_info=[]
         bran_ex=[]
+        compa=[] #compare total_voxel
+        ini_data=[]
         total_branch = []
+        length_ratio=[]
+        bran_local_in=[]
         i=0
         level=0
-        bran_info=[]
-        compa=[]
-        length_ratio=[]
-        ini_data=[]
-        bran_local_in=[]
         local=input("local_structure : ")
+
+
+        ##load_bran_info
+
+
         for var in braninfo:
             if var.startswith('Total branch point'):
                 uid = var.split()[5]
                 total_branch.append([uid])
-         #bran_name = bran_cp = bran_volength = bran_level = bran_parent = None
             elif var.startswith('TreeExample',21):
                 bran_name = var.split()[0]
                 bran_cp = var.split()[1]
@@ -69,6 +72,9 @@ def main ():
                 bran_parent = var.split()[13]
                 compa.append(int(bran_cp))#total-every-cp
                 bran_info.append([bran_name,bran_cp,bran_volength,bran_level,bran_parent])
+
+        ##local structure must < the CP# 
+
         compar=sorted(compa,key=float)
         if int(local) < 3:
             print("variable less than need")
@@ -78,13 +84,13 @@ def main ():
             print("min branch_length : "+ str(compar[0]))
             sys.exit(1)
         local=int(local) #local_structure
-        
+
+        ##initialize
+
         for var in bran:
             i=i+1
             if var.startswith("Parent ID = 0"):
-        #initialize
                 bran_ex_ID=bran[i-2]
-                #bran_ex_parentID=bran[i-1]
                 bran_ex_level=bran[i]
                 bran_ex_CP=bran[i+1]
                 bran_ex_CP=bran_ex_CP.split()
@@ -109,13 +115,22 @@ def main ():
                     bran_local_in_x.append(x)
                     bran_local_in_y.append(y)
                 level=1
+
+        ## input bran local number 
+
         bran_local_in.append([bran_local_in_z,bran_local_in_y,bran_local_in_x])
+
+        ## save the child ID
+
         def offspring(BP_CHild_ID):
             div=[]
             for var in BP_CHild_ID:
                 x=var.split()[3]
                 div.append(x)
             return div
+
+        ## write the information in txt
+        
         d.write("total_branch:")
         d.write(str(total_branch[0]))
         d.write("\n")
@@ -129,25 +144,24 @@ def main ():
         div=offspring(BP_CHild_ID)
         BP_CHild=[]
         child_le=[]
-        #倒敘
         d.write("to: ")
         d.write("\n")
-        ###先比較長度
-        #根據長度找哪個
-        #數據點
-        ####
-        XD=[]
+        XD=[] # save the Child ID
         bran_chi=[]
         #外面調用np.array
         #append([])
+
+
+        # utilize the child ID to find each  child information (利用ID 確保正確 )
+
+        
         for i in range(len(div)-1, -1, -1): 
             child=div.pop(i)
             a="ID = "
             a=a+str(int(child))+("\n")
             XD.append(a)
             t=0
-            #裡面調用list append
-            ### need _ 修改按照長度順序(將數據點也一併輸出)
+            
             for var in bran:
                 t=t+1
                 if var.startswith("ID") and a == bran[t-1]   :
@@ -160,22 +174,40 @@ def main ():
                     XD.append(voxel_length)
                     child_vlength=float(child_vlength)
                     child_le.append(child_vlength)
+
+                    
+        ###  sort child length from longest to smallest
+
+                    
         child_le=sorted(child_le,key=float,reverse=True)#finish sort
+
         #sort finish 再 load 一次 把 branch 拿出來
         bran_ex_vlength=float(bran_ex_vlength.split()[2])
         length_ratio.append([1])
+
+        ## finish  length ratio 
+
         for dd in child_le:
             length_ratio.append([float((float(dd))/bran_ex_vlength)])
+
+        ## write child with the order of length
+            
         for R in  child_le:
             for dd in range(len(XD)) :
                 if  XD[dd] == str(R):
-                    d.write(XD[dd-3])
-                    d.write(XD[dd-2])
-                    d.write(XD[dd-1])
-                    d.write(XD[dd])
+                    d.write(XD[dd-3]) # ID 
+                    d.write(XD[dd-2]) # Voxel 
+                    d.write(XD[dd-1]) # =
+                    d.write(XD[dd])   # length 
                     d.write("\n")
-                    bran_chi.append(XD[dd-3])
+                    bran_chi.append(XD[dd-3]) # record the ID  
+
+        
         bran_local_out=[]
+
+        ##  search the output local structure information 
+
+        
         for r in bran_chi:
             t=0
             for var in bran:
@@ -193,7 +225,11 @@ def main ():
                         bran_local_ch_x.append(x)
                         bran_local_ch_y.append(y)
             bran_local_out.append([bran_local_ch_z,bran_local_ch_x,bran_local_ch_y])
+        
         cof_init=[]
+
+        ##linear regression (in)
+        
         for leng in range(len(bran_local_in)) :
             ini=bran_local_in[leng]
             for lengt in range(len(ini)) :
@@ -204,6 +240,8 @@ def main ():
             cof_init.append(cof_ini)
         cof_out=[]
         
+        ##linear regression (output)
+        
         for leng in range(len(bran_local_out)) :
             ini=bran_local_out[leng]
             for lengt in range(len(ini)) :
@@ -213,7 +251,13 @@ def main ():
             cof_ini=OLS.main(x,y,z)
             q_br.put(cof_ini)
             cof_out.append([cof_ini])
+
+        ## theta record the local branch information 
+
         arc=[]
+
+        ## the theta from input to output
+        
         for leng in range(len(cof_init)) :
             for lengt in range(len(cof_out)):
                 coff=((cof_out[lengt])[:])
@@ -222,7 +266,9 @@ def main ():
                     theta=math.pi-theta
                 arc.append(theta)
         tt=q_br.qsize()
-        ##內積得後續的解(將裡面的計算)
+
+        ## the theta between output branches
+         
         def bran_inner(tt,cof_out) :
             for vr in range(tt-1) :
                 tr=q_br.get()
@@ -234,6 +280,9 @@ def main ():
                     arc.append(theta)
             q_br.get()
             return
+
+        ## write it to the txt
+        
         bran_inner(tt,cof_out)            
         d.write("\n")
         d.write("level zero : ")
@@ -259,6 +308,7 @@ def main ():
 ##放入queue
 ##小bug
 ###
+    
     def sorting(ini,q):
         child_le=ini[0]
         BP_CHild_ID=ini[1]
@@ -287,41 +337,57 @@ def main ():
         return t #總共幾個
 ###
 ##需要建立如果offspring ID=0 return
-##input(offspring)
+##output single offspring to advance data information 
 ###
-    def d_process_a(q,t):
+    def d_process_a(q):
         proce_ID=[]
-        for var in range(t):
+        tt=q.qsize()
+        for var in range(tt):
             proce_ID.append(q.get())
         if proce_ID == [] :
             return proce_ID
         return proce_ID
+
+
+###  
+    
     def d_process_ad(proce_ID,infor,level,compa,local):
         braninfo=infor[0]
         bran=infor[1]
         bran_info=[]
         bran_local_in=[]
         local
+        
+        ## in order to sort the length information and compare 
+
         for var in braninfo:
             if var.startswith('TreeExample',21):
                 bran_name = var.split()[0]
                 bran_cp = var.split()[1]
                 bran_volength = var.split()[4]
-                bran_level = var.split()[12]
-                bran_parent = var.split()[13]
-                bran_info.append([bran_name,bran_cp,bran_volength,bran_level,bran_parent])
+                bran_info.append([bran_name,bran_cp,bran_volength])
+        #        bran_level = var.split()[12]
+        #        bran_parent = var.split()[13]
+        #        bran_info.append([bran_name,bran_cp,bran_volength,bran_level,bran_parent])
+
+        
         if proce_ID == []:
             return
+
+        ##  save the child ID
+
         def offspring(BP_CHild_ID):
             div=[]
             for var in BP_CHild_ID:
                 x=var.split()[3]
                 div.append(x)
             return div
-        #倒敘
-        fk=0
+        
         d.write("\n")
         d.write("==============")
+
+        # get the general branch information 
+
         for ch in proce_ID:
             i=0
             length_ratio=[]
@@ -335,12 +401,15 @@ def main ():
             bran_local_in_z=[]
             bran_local_in_x=[]
             bran_local_in_y=[]
+
+            ## search branch from the data  
+
+            
             for var in bran:
                 i=i+1
-                if ch in var:#ID相同
-                    #print(ch)
+                if ch in var: # the branch ID  same as data ID 
+
                     bran_ex_ID=bran[i]
-                #bran_ex_parentID=bran[i-1]
                     bran_ex_level=bran[i+2]
                     bran_ex_CP=bran[i+3]
                     bran_ex_CP=bran_ex_CP.split()
@@ -348,13 +417,20 @@ def main ():
                     cp=int(bran_ex_CP[2])
                     BP_num=bran[cp+16+i]
                     bp=int(BP_num.split()[2])
+
+                    # add branch child ID for next iteration 
                     
                     for cha in range(1,bp+1):
     
                         BP_CHild_ID.append(bran[cp+17+i+bp+cha])
+
+                    # input branch number 
                     
                     for num in range(1,cp+1):
                         bran_ex_cp_num.append(bran[i+14+num])
+
+                    # input local structure  branch information 
+                        
                     for ws in range(i+14+cp,(i+14+cp-local),-1):
                         z=int(bran[ws].split()[0])
                         x=int(bran[ws].split()[1])
@@ -362,9 +438,13 @@ def main ():
                         bran_local_in_z.append(z)
                         bran_local_in_x.append(x)
                         bran_local_in_y.append(y)
+
+                    # search child ID and information 
+
                     div=offspring(BP_CHild_ID)
                     bran_chi=[]
                     XD=[]
+                    
                     for i in range(len(div)-1, -1, -1):
                         child=div.pop(i)
                         a="ID = "
@@ -385,13 +465,25 @@ def main ():
                                 child_vlength=float(child_vlength)
                                 child_le.append(child_vlength)
                                 child_le=sorted(child_le,key=float,reverse=True)#finish sort
+
+                    # finish sort with the order of branch length
+
+
                     bran_local_in.append([bran_local_in_z,bran_local_in_y,bran_local_in_x])
                     bran_ex_vlength_new=float(bran_ex_vlength.split()[2])
+
+                    ## if child_le = 0 mean the end branch should not print
+
+                    
                     if len(child_le) == 0 :
                         break
                     else :
                         level=level+1
                         length_ratio.append([1])
+
+                    
+                    ##print it 
+
                         for dd in child_le:
                             length_ratio.append([float((float(dd))/bran_ex_vlength_new)])
                         d.write("\n")
@@ -402,6 +494,10 @@ def main ():
                         d.write("\n")
                         d.write("to: ")
                         d.write("\n")
+
+                        
+                        ## write child with the order of length
+
                         for R in  child_le:
                             for dd in range(len(XD)) :
                                 if  XD[dd] == str(R):
@@ -411,6 +507,10 @@ def main ():
                                     d.write(XD[dd])
                                     bran_chi.append(XD[dd-3])
                                     d.write("\n")
+
+                        
+                        ## save the local branch output data 
+
                         bran_local_out=[]
                         for r in bran_chi:
                             t=0
@@ -430,6 +530,10 @@ def main ():
                                         bran_local_ch_y.append(y)
                             bran_local_out.append([bran_local_ch_z,bran_local_ch_x,bran_local_ch_y])
                         cof_init=[]
+
+                        ## local input branch linear regression
+
+                        
                         for leng in range(len(bran_local_in)) :
                             init=bran_local_in[leng]
                             for lengt in range(len(init)) :
@@ -439,6 +543,10 @@ def main ():
                             cof_ini=OLS.main(x,y,z)
                             cof_init.append(cof_ini)
                         cof_out=[]
+
+                        ## local output branch linear regression
+
+                        
                         for leng in range(len(bran_local_out)) :
                             init=bran_local_out[leng]
                             for lengt in range(len(init)) :
@@ -448,7 +556,13 @@ def main ():
                             cof_ini=OLS.main(x,y,z)
                             q_br.put(cof_ini)
                             cof_out.append([cof_ini])
+
+
                         arc=[]
+
+                        ## the theta from input to each output
+
+
                         for leng in range(len(cof_init)) :
                             for lengt in range(len(cof_out)):
                                 coff=((cof_out[lengt])[:])
@@ -457,7 +571,10 @@ def main ():
                                     theta=math.pi-theta
                                 arc.append(theta)
                         tt=q_br.qsize()
-                        ##內積得後續的解(將裡面的計算)
+
+                        
+                        ## the theta between output branches 
+
                         def bran_inner(tt,cof_out) :
                             for vr in range(tt-1) :
                                 tr=q_br.get()
@@ -469,7 +586,11 @@ def main ():
                                     arc.append(theta)
                             q_br.get()
                             return
-                        bran_inner(tt,cof_out)            
+                        bran_inner(tt,cof_out)
+
+                        # write data
+
+                        
                         d.write("\n")
                         d.write("level zero : ")
                         d.write(str(level))
@@ -490,12 +611,10 @@ def main ():
                         d.write("\n")
                         d.write("==============")
                         ini.append([child_le,BP_CHild_ID,bran_info,level])
-                        fk=fk+sorting_ad(ini,q)
-                    #return child_le,BP_CHild_ID,bran_info
-    
-        proce_ID=d_process_a(q,fk)  
+        # call iteration 
+        proce_ID=d_process_a(q)  
         d_process_ad(proce_ID,infor,level,compa,local)        
-#####call(offspring):length
+
 ###執行function
     
     infor=inpu()           
@@ -504,7 +623,7 @@ def main ():
     compa=ini[4]
     level=ini[3]
     t=sorting(ini,q)
-    proce_ID=d_process_a(q,t)
+    proce_ID=d_process_a(q)
     d_process_ad(proce_ID,infor,level,compa,local)
     d.close()
 if __name__ == "__main__":
